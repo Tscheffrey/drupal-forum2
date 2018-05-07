afd1e4d44f085a644df9ea069e15398c9e40833d
Hannes Schütz <hannes@fam-schuetz.de>
2018-05-07 13:09:49 +0200

    Minor CSS

---
+++ sites/default/files/php/twig/5aeafebe1e9c6_block--search-form-block.__f369FmJwFLqYkgHgKIwzXZIp/Px51WJC6BtJCxR2cl3KsWScFKVzXRw9QSOZ2g5ttDkI.php
@@ -0,0 +1,105 @@
+<?php
+
+/* themes/d8w3css/templates/block/block--search-form-block.html.twig */
+class __TwigTemplate_4cb8e8b59784822f765c8c3662a9863091111a577f0c6db0159c527b88374af3 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'content' => array($this, 'block_content'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 31, "block" => 35);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array('if', 'block'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 29
+        echo "
+  ";
+        // line 30
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title_prefix"] ?? null), "html", null, true));
+        echo "
+  ";
+        // line 31
+        if (($context["label"] ?? null)) {
+            // line 32
+            echo "    <h2";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title_attributes"] ?? null), "html", null, true));
+            echo ">";
+            echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["label"] ?? null), "html", null, true));
+            echo "</h2>
+  ";
+        }
+        // line 34
+        echo "  ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["title_suffix"] ?? null), "html", null, true));
+        echo "
+  ";
+        // line 35
+        $this->displayBlock('content', $context, $blocks);
+    }
+
+    public function block_content($context, array $blocks = array())
+    {
+        // line 36
+        echo "    ";
+        echo $this->env->getExtension('Twig_Extension_Sandbox')->ensureToStringAllowed($this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, ($context["content"] ?? null), "html", null, true));
+        echo "
+  ";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/d8w3css/templates/block/block--search-form-block.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  72 => 36,  66 => 35,  61 => 34,  53 => 32,  51 => 31,  47 => 30,  44 => 29,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "themes/d8w3css/templates/block/block--search-form-block.html.twig", "/Users/Hannes/Documents/development/htdocs/drupal3/themes/d8w3css/templates/block/block--search-form-block.html.twig");
+    }
+}
